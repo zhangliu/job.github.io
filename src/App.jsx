01a3c8b0240b13fdb1dce6e2adb4cf0d8f30974a
zhangyunnian <zhangyunnian@bytedance.com>
2020-06-06 15:16:27 +0800

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Loadable from 'react-loadable';
 
 const params  = new URLSearchParams(window.location.search);
 const pageName = params.get('page') || 'index';
 
-const Page = Loadable({
-  loader: () => import(`./pages/${pageName}`),
-  loading: () => <div className="ta:c mt:20">加载中...</div>,
-});
+const Page = React.lazy(() => import(`./pages/${pageName}`));
 
 function App() {
-  return (<Page />);
+  return (
+    <React.Suspense fallback={<div className="ta:c mt:20">加载中...</div>}>
+      <Page />
+    </React.Suspense>
+  );
 }
 
 export default App;
