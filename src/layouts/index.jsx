c453c45b514b3df03866c6ba65981224a0833b13
zhangyunnian <zhangyunnian@bytedance.com>
2020-06-14 13:42:01 +0800

    modify

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Sidebar from './sidebar';
+import Sidebar from './sidebar';
 
 export default class ClassName extends React.Component{
   componentDidMount() {
@@ -8,12 +8,14 @@ export default class ClassName extends React.Component{
 
   render() {
     return (
-      <div className="p:a t:0 r:0 b:0 l:0">
+      <div className="p:a t:0 r:0 b:0 l:0 bgc:eff0f1">
         {/* <div id="header"></div> */}
-        {/* <div id='sidebar'><Sidebar /></div> */}
-        <div id='content' className="bgc:eff0f1 d:f jc:c ai:c h:100%">
-          <div className="h:80%">
-            {this.props.children}
+        <div className="d:f p:a t:0 r:0 b:0 l:0">
+          <div id='sidebar' className="w:240 bgc:fff bw:0 brw:1 bs:s bc:dee0e3"><Sidebar /></div>
+          <div id='content' className="d:f jc:c ai:c w:100%">
+            <div className="h:80%">
+              {this.props.children}
+            </div>
           </div>
         </div>
       </div>
--- src/layouts/sidebar.jsx
+++ src/layouts/sidebar.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { sleep } from '../utils/time';
 
 export default class ClassName extends React.Component{
+  state = { pages: [] };
+  async componentDidMount() {
+    await waitPage();
+    const pages = getPages();
+    const doc = this.iframe.contentWindow.document;
+    const heads = Array.from(document.head.children);
+    
+    heads.forEach(head => doc.head.appendChild(head.cloneNode(true)));
+    pages.forEach(page => {
+      const node = page.cloneNode(true);
+      node.style.display = 'block';
+      node.style.transform = 'scale(0.5)';
+      window.$(node).show();
+      doc.body.appendChild(node);
+    });
+  }
+
   render() {
     return (
-      <div>siderbar</div>
+      <div className="h:100%">
+        <iframe ref={node => this.iframe = node} title="iframe" className="h:100% w:100% bw:1 of:a" />
+      </div>
     );
   }
+}
+
+async function waitPage() {
+  while(true) {
+    await sleep(100);
+    const pages = getPages();
+    if (pages.length > 0) return;
+  }
+}
+
+function getPages() {
+  return document.querySelectorAll('#content .page');
 }
